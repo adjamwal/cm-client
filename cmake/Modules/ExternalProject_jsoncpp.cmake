8bd20ff7855141518565f4f8c74f333851fabbfb
Louis Lee <louilee@cisco.com>
2022-08-22 15:49:07 -0600

    First example of using cmake to build external third-party libraries

---
+++ cmake/Modules/ExternalProject_jsoncpp.cmake
@@ -0,0 +1,11 @@
+# jsoncpp external project build file
+# Copyright Cisco Systems, Inc. 2022
+
+include(ExternalProject)
+
+ExternalProject_Add(
+    third-party-jsoncpp
+    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third-party/jsoncpp/jsoncpp
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/jsoncpp/export
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_CURRENT_SOURCE_DIR}/third-party/jsoncpp/export
+)
