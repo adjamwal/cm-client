0bda89592da194dfc480a353c892e62860435b88
Louis Lee <louilee@cisco.com>
2022-08-26 16:22:03 -0600

    Point to CMID where install is defined to specify proper exports

--- cmake/Modules/ExternalProject_cmid.cmake
+++ cmake/Modules/ExternalProject_cmid.cmake
@@ -7,9 +7,9 @@ include(ExternalProject)
 # TODO Be more specific about tool chain
 # TODO Adapt to make OSX options based on target
 # TODO Update CMAKE_COMMAND to take in actual version numbers
-# TODO Currently project has no rules to install, so INSTALL_COMMAND is missing below
-#    cp ${CMAKE_CURRENT_BINARY_DIR}/cmid/src/third-party-cmid-build/agent/csc_cmid ${CM_THIRDPARTY_EXPORT}/bin/.
-#    cp ${CMAKE_CURRENT_SOURCE_DIR}/EndpointIdentity/cmid/controlplugin/CMIDAgentController.h ${CM_THIRDPARTY_EXPORT}/include/.
+# TODO Currently project has no rules to install, add INSTALL_COMMAND (such as below)
+#      Right now, it's been forked and we added our own install directives
+#    INSTALL_COMMAND cp ${CMAKE_CURRENT_BINARY_DIR}/cmid/src/third-party-cmid-build/controlplugin/libcmidcontrolplugin.dylib ${CM_THIRDPARTY_EXPORT}/lib/.
 ExternalProject_Add(
     third-party-cmid
     SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/EndpointIdentity/cmid
@@ -21,5 +21,4 @@ ExternalProject_Add(
         -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES_}
         -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}
     CMAKE_COMMAND VERSION=1.1 RELNUM=1111 ${CMAKE_COMMAND}
-    INSTALL_COMMAND cp ${CMAKE_CURRENT_BINARY_DIR}/cmid/src/third-party-cmid-build/controlplugin/libcmidcontrolplugin.dylib ${CM_THIRDPARTY_EXPORT}/lib/.
 )
