80632979875ef22da5976995461f8a016111c519
Priyanka Ramesh <priyrame@cisco.com>
2023-01-25 11:45:16 +0530

    Included all debug defines in ConfigTest cmake

--- client/tests/Config/ConfigTest.cpp
+++ client/tests/Config/ConfigTest.cpp
@@ -33,15 +33,11 @@ public:
     ConfigData testdata;
     ConfigTest()
     {
+        std::remove( TEST_CONFIG_FILEPATH );
         testdata = GetParam();
         std::ofstream out_file( Config::cmConfigPath + "/" + TEST_CONFIG_FILEPATH, std::ios::out );
         out_file << testdata.data;
         out_file.close();
-    }
-
-    static void TearDownTestSuite()
-    {
-       std::remove( TEST_CONFIG_FILEPATH );
     }
 };
 
@@ -55,15 +51,15 @@ INSTANTIATE_TEST_SUITE_P( Config,
                           ConfigTest,
                           ::testing::Values( ConfigData{"{\"id\": {\"enable_verbose_logs\": true},\"pm\": {\"loglevel\": 7,\"CheckinInterval\": 300000,\"MaxStartupDelay\": 2000,\"maxFileCacheAge_s\": 604800,\"AllowPostInstallReboots\": true},\"uc\": {\"loglevel\": 7}}",CM_LOG_LVL_T::CM_LOG_DEBUG,"DEBUG"},
                                              ConfigData{"{\"id\": {\"enable_verbose_logs\": true},\"pm\": {\"loglevel\": 7,\"CheckinInterval\": 300000,\"MaxStartupDelay\": 2000,\"maxFileCacheAge_s\": 604800,\"AllowPostInstallReboots\": true},\"uc\": {\"loglevel\": 5}}",CM_LOG_LVL_T::CM_LOG_NOTICE,"NOTICE"},
-                                             ConfigData{"{\"id\": {\"enable_verbose_logs\": true},\"pm\": {\"loglevel\": 7,\"CheckinInterval\": 300000,\"MaxStartupDelay\": 2000,\"maxFileCacheAge_s\": 604800,\"AllowPostInstallReboots\": true},\"uc\": {\"loglevel\": 10}}",DEFAULT_LOG_LEVEL,"DEAFULT"},
-                                             ConfigData{"{\"id\": {\"enable_verbose_logs\": true},\"uc\": {\"log_level\": 3}}",DEFAULT_LOG_LEVEL,"DEAFULT"},
+                                             ConfigData{"{\"id\": {\"enable_verbose_logs\": true},\"pm\": {\"loglevel\": 7,\"CheckinInterval\": 300000,\"MaxStartupDelay\": 2000,\"maxFileCacheAge_s\": 604800,\"AllowPostInstallReboots\": true},\"uc\": {\"loglevel\": 10}}",DEFAULT_LOG_LEVEL,"DEFAULT"},
+                                             ConfigData{"{\"id\": {\"enable_verbose_logs\": true},\"uc\": {\"log_level\": 3}}",DEFAULT_LOG_LEVEL,"DEFAULT"},
                                              ConfigData{"{\"uc\": {\"loglevel\": 5}}",CM_LOG_LVL_T::CM_LOG_NOTICE,"NOTICE"},
-                                             ConfigData{"{\"id\": {\"enable_verbose_logs\": true},\"loglevel\": 3}",DEFAULT_LOG_LEVEL,"DEAFULT"},
-                                             ConfigData{"{}",DEFAULT_LOG_LEVEL,"DEAFULT"},
-                                             ConfigData{"{\"uc\": {\"loglevel\":}}",DEFAULT_LOG_LEVEL,"DEAFULT"},
-                                             ConfigData{"",DEFAULT_LOG_LEVEL,"DEAFULT"},
-                                             ConfigData{"[\"test\":123]",DEFAULT_LOG_LEVEL,"DEAFULT"},
-                                             ConfigData{"{\"loglevel\":4}",DEFAULT_LOG_LEVEL,"DEAFULT"} ) );
+                                             ConfigData{"{\"id\": {\"enable_verbose_logs\": true},\"loglevel\": 3}",DEFAULT_LOG_LEVEL,"DEFAULT"},
+                                             ConfigData{"{}",DEFAULT_LOG_LEVEL,"DEFAULT"},
+                                             ConfigData{"{\"uc\": {\"loglevel\":}}",DEFAULT_LOG_LEVEL,"DEFAULT"},
+                                             ConfigData{"",DEFAULT_LOG_LEVEL,"DEFAULT"},
+                                             ConfigData{"[\"test\":123]",DEFAULT_LOG_LEVEL,"DEFAULT"},
+                                             ConfigData{"{\"loglevel\":4}",DEFAULT_LOG_LEVEL,"DEFAULT"} ) );
 
 TEST_P( ConfigTest, getLogLevel )
 {
