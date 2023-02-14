55e19fa9b0ba43f741274694008983c04d7309be
asthakra <asthakra@cisco.com>
2023-02-14 13:34:11 +0530

    fix for jenkins build failure with recent change, adding default CM_BUILD_VER in build script to be removed when we add CM_BUILD_VER to jenkins build env

--- scripts/build.sh
+++ scripts/build.sh
@@ -144,8 +144,10 @@ else
         echo " building CM Installer ..."
         export BUILD_TYPE="${CMAKE_BUILD_DIR}"
         if [ "x$CM_BUILD_VER" = "x" ]; then
-            echo "CM_BUILD_VER not set. exiting..."
-            exit 1
+            echo "CM_BUILD_VER not set."
+            #TODO : add CM_BUILD_VER to jenkins env 
+            export CM_BUILD_VER="1.0.0000" 
+            #exit 1
         fi
         DMG_DIR="Installer"
         DMG_SCRIPT="build_cm_installer.sh"
