43541031162ad9f1f4d325be03d780754ce15a2f
adisaini <acdev@localhost.localdomain>
2025-01-15 01:13:37 -0500

    API modifications for Uninstall and enum definition for Identifier Type

--- OSPackageManager/linux/IPackageUtil.hpp
+++ OSPackageManager/linux/IPackageUtil.hpp
@@ -9,6 +9,12 @@ struct PackageInfo {
     std::string packageName;
     std::string version;
 };
+typedef enum _PACKAGE_IDENTIFIER_TYPE_T
+{
+    PACKAGE_IDENTIFIER_BY_NAME = 0,
+    PACKAGE_IDENTIFIER_BY_NEVRA = 1
+
+}PACKAGE_IDENTIFIER_TYPE_T;
 
 class PackageUtilException : public std::runtime_error {
 public:
@@ -20,8 +26,8 @@ public:
     virtual ~IPackageUtil() = default;
     
     virtual std::vector<std::string> listPackages() const = 0;
-    virtual PackageInfo getPackageInfo(const std::string& identifierType, const std::string& packageIdentifier) const = 0;
-    virtual std::vector<std::string> listPackageFiles(const std::string& identifierType, const std::string& packageIdentifier) const = 0;
+    virtual PackageInfo getPackageInfo(const PACKAGE_IDENTIFIER_TYPE_T& identifierType, const std::string& packageIdentifier) const = 0;
+    virtual std::vector<std::string> listPackageFiles(const PACKAGE_IDENTIFIER_TYPE_T& identifierType, const std::string& packageIdentifier) const = 0;
     virtual bool installPackage(const std::string& packagePath, const std::map<std::string, int>&  installOptions = {}) const = 0;
-    virtual bool uninstallPackage(const std::string& identifierType, const std::string& packageIdentifier) const = 0;
+    virtual bool uninstallPackage(const std::string& packageIdentifier) const = 0;
 };
