38cd54aa5aaaf4c8e5761046de6c773fa35ddacc
adisaini <acdev@localhost.localdomain>
2025-02-06 07:57:26 -0500

    Better cleanup mechanism via use of smart pointers rather than goto

--- OSPackageManager/common/CommandExec.cpp
+++ OSPackageManager/common/CommandExec.cpp
@@ -7,104 +7,115 @@
 #include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <memory>
 
-int CommandExec::ExecuteCommand(char *cmd, char * const *argv, int *exitCode)
-{
+int CommandExec::ExecuteCommand(char *cmd, char * const *argv, int *exitCode) {
     int ret = -1;
     int status = 1;
     pid_t pid = -1;
     pid_t waitPid = -1;
     extern char **environ;
 
-    do {
-        if (!cmd || !*cmd || !argv || '/' != cmd[0]) {
-            errno = EINVAL;
-            break;
+    if (!cmd || !*cmd || !argv || '/' != cmd[0]) {
+        errno = EINVAL;
+        return ret;
+    }
+
+    if (posix_spawn(&pid, cmd, NULL, NULL, argv, environ) != 0) {
+        PM_LOG_ERROR("posix_spawn failed: %s", cmd);
+        return ret;
+    }
+
+    PM_LOG_DEBUG("Spawned process for cmd %s: %d", cmd, pid);
+
+    while ((waitPid = waitpid(pid, &status, 0)) == -1 && errno == EINTR)
+        ;
+    if (waitPid == -1) {
+        PM_LOG_ERROR("waitpid failed: %d with error code: %d", pid, errno);
+        return ret;
+    }
+
+    if (WIFEXITED(status)) {
+        ret = 0;
+        PM_LOG_DEBUG("Process terminated normally: %d (exit code: %d)", pid, WEXITSTATUS(status));
+        if (exitCode) {
+            *exitCode = WEXITSTATUS(status);
         }
+        return ret;
+    }
 
-        if (posix_spawn(&pid, cmd, NULL, NULL, argv, environ) != 0) {
-            PM_LOG_ERROR("posix_spawn failed: %s", cmd);
-            break;
-        }
+    errno = ESRCH;
 
-        PM_LOG_DEBUG("Spawned process for cmd %s: %d", cmd, pid);
-
-        while ((waitPid = waitpid(pid, &status, 0)) == -1 && errno == EINTR)
-            ;
-        if (waitPid == -1) {
-            PM_LOG_ERROR("waitpid failed: %d", pid);
-            break;
-        }
-
-        if (WIFEXITED(status)) {
-            ret = 0;
-            PM_LOG_DEBUG("Process terminated normally: %d (exit code: %d)", pid, WEXITSTATUS(status));
-            if (exitCode) {
-                *exitCode = WEXITSTATUS(status);
-            }
-            break;
-        }
-
-        errno = ESRCH;
-
-        if (WIFSIGNALED(status)) {
-            PM_LOG_ERROR( "Process terminated due to uncaught exception: %d",pid);
-        } else if (WIFSTOPPED(status)) {
-            PM_LOG_ERROR( "Process stopped abnormally: %d", pid);
-        } else {
-            PM_LOG_ERROR( "Process did not return: %d", pid);
-        }
-    } while (0);
+    if (WIFSIGNALED(status)) {
+        PM_LOG_ERROR( "Process terminated due to uncaught exception: %d",pid);
+    } else if (WIFSTOPPED(status)) {
+        PM_LOG_ERROR( "Process stopped abnormally: %d", pid);
+    } else {
+        PM_LOG_ERROR( "Process did not return: %d", pid);
+    }
 
     return ret;
 }
 
-int CommandExec::ExecuteCommandCaptureOutput(char *cmd, char * const *argv, int *exitCode, std::string &output)
-{
+int CommandExec::ExecuteCommandCaptureOutput(char *cmd, char * const *argv, int *exitCode, std::string &output) {
     int ret = -1;
     int status = 1;
     pid_t pid = -1;
     pid_t waitPid = -1;
     extern char **environ;
     posix_spawn_file_actions_t childFdActions = {0};
-    bool childFdActions_init = false;
-    int out[2] = { -1, -1 };
+    int out [2] = {-1, -1};
     ssize_t bytesRead = 0;
-    fd_set readFds;
-    FD_ZERO(&readFds);
-    const int waitPidOptions = 0;
-    const struct timespec selectTimeout = { .tv_nsec = 10000000 };
-    int flags = fcntl(out[0], F_GETFL, 0);
-    
+    const int waitPidOptions = 0;   
 
     if (!cmd || !*cmd || !argv || '/' != cmd[0]) {
-        goto done;
+        return ret;
     }
+
+    // Lambda Function to delete file actions (Used as custom deleter)
+    auto fileActionsDeleter = [](posix_spawn_file_actions_t *actions) {
+        posix_spawn_file_actions_destroy(actions);
+    };                                                                      
 
     if (posix_spawn_file_actions_init(&childFdActions) != 0) {
         PM_LOG_ERROR("posix_spawn_file_actions_init failed");
-        goto done;
+        return ret;
     }
-    childFdActions_init = true;
+
+    auto childFdActionsPtr = std::unique_ptr<posix_spawn_file_actions_t, decltype(fileActionsDeleter)>(&childFdActions, fileActionsDeleter); // Just for cleaning purpose on return.
+
+    // Lambda Function to close pipe (Used as custom deleter)
+    auto close_pipe = [](int *out) {
+        if (out[0] != -1) {
+            (void)close(out[0]);
+        }
+        if (out[1] != -1) {
+            (void)close(out[1]);
+        }
+    };
+
+    auto outPtr = std::unique_ptr<int, decltype(close_pipe)>(out, close_pipe); // Just for cleaning purpose on return.
+
+    int flags = fcntl(out[0], F_GETFL, 0); 
 
     if (pipe(out) == -1) {
         PM_LOG_ERROR("pipe failed");
-        goto done;
+        return ret;
     }
 
     if (posix_spawn_file_actions_adddup2(&childFdActions, out[1], 1) != 0) {
         PM_LOG_ERROR("posix_spawn_file_actions_adddup2");
-        goto done;
+        return ret;
     }
 
     if (posix_spawn_file_actions_addclose(&childFdActions, out[0]) != 0) {
         PM_LOG_ERROR("posix_spawn_file_actions_addclose");
-        goto done;
+        return ret;
     }
 
     if (posix_spawn(&pid, cmd, &childFdActions, NULL, argv, environ) != 0) {
         PM_LOG_ERROR("posix_spawn failed: %s", cmd);
-        goto done;
+        return ret;
     }
 
     PM_LOG_DEBUG("Spawned process for cmd %s: %d", cmd, pid);
@@ -116,31 +127,19 @@ int CommandExec::ExecuteCommandCaptureOutput(char *cmd, char * const *argv, int 
     } while (waitPid < 0 && errno == EINTR);
     
     if (waitPid < 0) {
-        PM_LOG_ERROR("waitpid failed");
-        goto done;
+        PM_LOG_ERROR("waitpid failed with error: %d", errno);
+        return ret;
     } else if (waitPid > 0) {
         if (WIFEXITED(status)) {
-            int err;
-
             PM_LOG_DEBUG("Process '%s' terminated normally: %d (exit code: %d)", cmd, pid, WEXITSTATUS(status));
-
-            FD_ZERO(&readFds);
-            FD_SET(out[0], &readFds);
+            
             output.clear();
 
-            while ((err = pselect(out[0] + 1, &readFds, NULL, NULL, &selectTimeout, NULL)) < 0 &&
-                   errno == EINTR)
-                ;
-            if (err < 0) {
-                PM_LOG_ERROR("pselect failed");
-                goto done;
-            } else if (err == 1) {
-                char buffer[1024];
-                while ((bytesRead = read(out[0], buffer, sizeof(buffer) - 1)) > 0) {
-                    buffer[bytesRead] = '\0';
-                    output.append(buffer);                       
-                }
-            }
+            char buffer[1024];
+            while ((bytesRead = read(out[0], buffer, sizeof(buffer) - 1)) > 0) {
+                buffer[bytesRead] = '\0';
+                output.append(buffer);
+            }                       
 
             if (exitCode) {
                 *exitCode = WEXITSTATUS(status);
@@ -155,22 +154,10 @@ int CommandExec::ExecuteCommandCaptureOutput(char *cmd, char * const *argv, int 
         }
     }
 
-done:
-    if (out[0] != -1) {
-        (void)close(out[0]);
-    }
-    if (out[1] != -1) {
-        (void)close(out[1]);
-    }
-    if (childFdActions_init) {
-        posix_spawn_file_actions_destroy(&childFdActions);
-    }
-
     return ret;
 }
 
-void CommandExec::ParseOutput(const std::string &output, std::vector<std::string> &outputLines)
-{
+void CommandExec::ParseOutput(const std::string &output, std::vector<std::string> &outputLines) {
     size_t start = 0, end = 0;
     while ((end = output.find('\n', start)) != std::string::npos) {
         outputLines.push_back(output.substr(start, end - start));
