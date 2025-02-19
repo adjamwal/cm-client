3a516a8dcd90a04ca5ebdad02ef714a017fd5648
Priyanka Ramesh <priyrame@cisco.com>
2025-02-19 11:43:09 +0530

    checking in placeholder pkg utils for debian

--- OSPackageManager/linux/PmPlatformDependencies.cpp
+++ OSPackageManager/linux/PmPlatformDependencies.cpp
@@ -9,15 +9,19 @@
 #include "FileUtilities.hpp"
 #ifdef IS_RHEL
 #include "PackageUtilRPM.hpp"
+#else
+#include "PackageUtilDEB.hpp"
 #endif
 
 PmPlatformDependencies::PmPlatformDependencies()
+        :
 #ifdef IS_RHEL
-	    :
-        pmPkgUtil_(std::make_shared<PackageUtilRPM>())
-        , pmConfiguration_ { PmPlatformConfiguration(std::make_shared<CMIDAPIProxy>()) }
-        , pmComponentManager_{PmPlatformComponentManager(pmPkgUtil_, std::make_shared<PackageManager::FileUtilities>())}
+        pmPkgUtil_(std::make_shared<PackageUtilRPM>()),
+#else
+        pmPkgUtil_(std::make_shared<PackageUtilDEB>()),
 #endif
+        pmConfiguration_ { PmPlatformConfiguration(std::make_shared<CMIDAPIProxy>()) },
+        pmComponentManager_{PmPlatformComponentManager(pmPkgUtil_, std::make_shared<PackageManager::FileUtilities>())}
 {}
 
 IPmPlatformConfiguration &PmPlatformDependencies::Configuration()
